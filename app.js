ad7d4e8bbee64d4a9a3decb8a2b407c91569dfe7
Lukasz <lukasz.owczarek@enp.pl>
2020-03-05 11:19:27 +0100

    express init

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const path = require('path');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.use(express.static(path.join(__dirname, 'public')));
+
+module.exports = app;
